68b624ae4a6a18215d0d81a2104f13dee50617c6
Alexander <vasiljevaa@mail.ru>
2017-05-14 23:34:07 +0300

    add instrumentation for agent and attribute to manifest for jarTask

--- src/main/kotlin/agent/Agent.kt
+++ src/main/kotlin/agent/Agent.kt
@@ -1,5 +1,8 @@
 package agent
 
+import jdk.internal.org.objectweb.asm.ClassReader
+import jdk.internal.org.objectweb.asm.ClassWriter
+import transformer.BeforeInvokeVisitor
 import java.lang.instrument.Instrumentation
 
 class Agent {
@@ -7,6 +10,13 @@ class Agent {
         @JvmStatic
         fun premain(agentArgs: String?, inst: Instrumentation) {
             println("Agent started.")
+            inst.addTransformer ({ _, _, _, _, buffer ->
+                val cw = ClassWriter(ClassWriter.COMPUTE_FRAMES)
+                val cv = BeforeInvokeVisitor(cw)
+                val reader = ClassReader(buffer)
+                reader.accept(cv, 0)
+                cw.toByteArray()
+            }, true)
         }
     }
 }
